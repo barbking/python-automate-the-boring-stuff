d959b1141e008272df43436a0c490d30aded6b30
Barbara King <barbgross@aol.com>
2020-01-12 23:27:20 -0600

    webbrowser module

--- WebScraping.py
+++ WebScraping.py
@@ -4,5 +4,17 @@
 #selenium Launches and controls a web browser. The selenium module is able to fill
 #in forms and simulate mouse clicks in this browser.
 
-import webbrowser
+import webbrowser, sys
 webbrowser.open('https://inventwithpython.com/')
+
+sys.argv
+#! python3
+# mapIt.py - Launches a map in the browser using an address from the
+# command line or clipboard.
+
+import webbrowser, sys
+if len(sys.argv) > 1:
+    # Get address from command line.
+    address = ' '.join(sys.argv[1:])
+
+# TODO: Get address from clipboard.
