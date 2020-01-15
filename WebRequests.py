34caa8e4b4389c8cedd27157cbecbc4e7779f3e6
Barbara King <barbgross@aol.com>
2020-01-14 22:54:14 -0600

    requests

--- WebRequests.py
+++ WebRequests.py
@@ -7,3 +7,28 @@ print(res.status_code == requests.codes.ok)
 print(len(res.text))
 # 178978
 print(res.text[:250])
+
+# raise error if bad request
+res = requests.get('https://inventwithpython.com/page_that_does_not_exist')
+# ensure that a program halts if a bad download occurs, or wrap in exception to cont run
+res.raise_for_status()
+
+# using exception
+import requests
+res = requests.get('https://inventwithpython.com/page_that_does_not_exist')
+try:
+    res.raise_for_status()
+except Exception as exc:
+    print('There was a problem: %s' % (exc))
+
+# write webpage to file
+import requests
+res = requests.get('https://automatetheboringstuff.com/files/rj.txt')
+res.raise_for_status()
+playFile = open('RomeoAndJuliet.txt', 'wb')
+for chunk in res.iter_content(100000):
+    playFile.write(chunk)
+playFile.close()
+
+# for more info on requests
+# requests.readthedoc.org
